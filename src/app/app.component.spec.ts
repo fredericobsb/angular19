338b5c837d88ab744a3152e718614090d660349a
Frederico <frederico0707@gmail.com>
2025-05-22 13:19:28 -0300

    antes mat-form-field

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent,  RouterModule.forRoot([]),],
     }).compileComponents();
   });
 
@@ -24,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, crud-angular19');
+    expect(compiled).toBeDefined();
   });
 });
---
+++ src/app/cadastro/cadastro.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+//import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { CadastroComponent } from './cadastro.component';
+
+describe('CadastroComponent', () => {
+  let component: CadastroComponent;
+  let fixture: ComponentFixture<CadastroComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CadastroComponent],
+      // providers: [
+     //   provideAnimations()
+    //]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CadastroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
