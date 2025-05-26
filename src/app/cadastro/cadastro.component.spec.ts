d6ca7ad4d002ed71849596472608e8f9405a9c68
Frederico Lopes Ramos <frederico0707@gmail.com>
2025-05-26 19:22:19 -0300

    Testes para edicao de cliente ok.

--- src/app/cadastro/cadastro.component.spec.ts
+++ src/app/cadastro/cadastro.component.spec.ts
@@ -4,6 +4,8 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { CadastroComponent } from './cadastro.component';
 import { Cliente } from './cliente';
 import { ClienteService } from '../cliente.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('CadastroComponent', () => {
   let component: CadastroComponent;
@@ -15,7 +17,15 @@ describe('CadastroComponent', () => {
       imports: [CadastroComponent],
        providers: [
         provideAnimations(),
-        ClienteService// garante que o service será injetado
+        ClienteService,// garante que o service será injetado
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParamMap: of({
+              params: {id: 1}  // Pode simular params aqui se quiser testar outros casos
+            })
+          }
+        }
       ]
     })
     .compileComponents();
@@ -33,6 +43,32 @@ describe('CadastroComponent', () => {
   it('should save a cliente', () =>{
    const spy = spyOn(clienteService, 'salvar');
    component.salvar();
-   expect(spy).toHaveBeenCalledWith(component.cliente);
+   expect(spy).toHaveBeenCalled();
   })
+
+  it('should set atualizando to true and set cliente when id is found', () => {
+    const mockCliente = Cliente.newCliente();
+    const id = mockCliente.id;
+
+    // Mock do ActivatedRoute para retornar um id
+    const activatedRoute = TestBed.inject(ActivatedRoute) as any;
+    activatedRoute.queryParamMap = of({
+      params: { id: id }
+    });
+
+    // Spy no clienteService para retornar um cliente
+    spyOn(clienteService, 'buscarClientePorId').and.returnValue(mockCliente);
+
+    component.ngOnInit();
+
+    expect(component.atualizando).toBeTrue();
+    expect(component.cliente).toEqual(mockCliente);
+  });
+
+  it('should update cliente when atualizando is true', () => {
+    component.atualizando = true;
+    const spy = spyOn(clienteService, 'atualizar');
+    component.salvar();
+    expect(spy).toHaveBeenCalledWith(component.cliente);
+  });
 });
